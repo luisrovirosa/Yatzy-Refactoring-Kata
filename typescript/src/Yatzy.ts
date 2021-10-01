6217dace81f8a41c8eefa011fd139bf2a8bd16e3
carlos.castillo <carlos.castillo@cabify.com>
2021-10-01 18:54:25 +0200

    Refactor twos method

--- typescript/src/Yatzy.ts
+++ typescript/src/Yatzy.ts
@@ -26,13 +26,9 @@ export default class Yatzy {
   }
 
   static twos(d1: number, d2: number, d3: number, d4: number, d5: number): number {
-    var sum = 0;
-    if (d1 == 2) sum += 2;
-    if (d2 == 2) sum += 2;
-    if (d3 == 2) sum += 2;
-    if (d4 == 2) sum += 2;
-    if (d5 == 2) sum += 2;
-    return sum;
+    return [d1, d2, d3, d4, d5]
+        .filter(number => number === 2)
+        .reduce((acum, number) => acum + number, 0);
   }
 
   static threes(d1: number, d2: number, d3: number, d4: number, d5: number): number {
