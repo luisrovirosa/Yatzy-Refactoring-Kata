f8ee835be0e260b19e5998dbd0c88b6ed46da4a5
carlos.castillo <carlos.castillo@cabify.com>
2021-10-01 19:30:13 +0200

    Refactor score_pair method

--- typescript/src/Yatzy.ts
+++ typescript/src/Yatzy.ts
@@ -40,15 +40,16 @@ export default class Yatzy {
   }
 
   static score_pair(d1: number, d2: number, d3: number, d4: number, d5: number): number {
-    var counts = [0, 0, 0, 0, 0, 0, 0, 0, 0];
-    counts[d1 - 1]++;
-    counts[d2 - 1]++;
-    counts[d3 - 1]++;
-    counts[d4 - 1]++;
-    counts[d5 - 1]++;
-    var at;
-    for (at = 0; at != 6; at++) if (counts[6 - at - 1] >= 2) return (6 - at) * 2;
-    return 0;
+    const occurrenceComputation = [6, 5, 4, 3, 2, 1]
+        .map((x) => ({
+          diceNumber: x,
+          occurrences: [d1, d2, d3, d4, d5].filter(n => n === x).length
+        }))
+        .filter(({occurrences}) => occurrences >= 2);
+    if (!occurrenceComputation[0]) return 0;
+
+    const {diceNumber, occurrences} = occurrenceComputation[0]
+    return diceNumber * occurrences;
   }
 
   static two_pair(d1: number, d2: number, d3: number, d4: number, d5: number): number {
