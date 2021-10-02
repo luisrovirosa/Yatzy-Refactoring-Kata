50f1b96015432c578d4b82347f98e1a94299c203
Luis Rovirosa Mairlot <luis@codium.team>
2021-10-02 21:51:15 +0200

    [refactor] largeStraight

--- typescript/src/Yatzy.ts
+++ typescript/src/Yatzy.ts
@@ -62,16 +62,8 @@ export default class Yatzy {
     return this.sameRolls(roll as Roll, [1, 2, 3, 4, 5] as Roll) ? 15 : 0;
   }
 
-  static largeStraight(d1: number, d2: number, d3: number, d4: number, d5: number): number {
-    var tallies;
-    tallies = [0, 0, 0, 0, 0, 0, 0, 0];
-    tallies[d1 - 1] += 1;
-    tallies[d2 - 1] += 1;
-    tallies[d3 - 1] += 1;
-    tallies[d4 - 1] += 1;
-    tallies[d5 - 1] += 1;
-    if (tallies[1] == 1 && tallies[2] == 1 && tallies[3] == 1 && tallies[4] == 1 && tallies[5] == 1) return 20;
-    return 0;
+  static largeStraight(...roll: number[]): number {
+    return this.sameRolls(roll as Roll, [2, 3, 4, 5, 6] as Roll) ? 20 : 0;
   }
 
   static fullHouse(d1: number, d2: number, d3: number, d4: number, d5: number): number {
