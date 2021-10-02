a8fac6e4b3fd423d60d98a9ec53b412df968971f
Luis Rovirosa Mairlot <luis@codium.team>
2021-10-02 20:03:28 +0200

    two_pair making the the tests pass

--- typescript/src/Yatzy.ts
+++ typescript/src/Yatzy.ts
@@ -48,21 +48,12 @@ export default class Yatzy {
   }
 
   static two_pair(d1: number, d2: number, d3: number, d4: number, d5: number): number {
-    var counts = [0, 0, 0, 0, 0, 0, 0, 0, 0];
-    counts[d1 - 1]++;
-    counts[d2 - 1]++;
-    counts[d3 - 1]++;
-    counts[d4 - 1]++;
-    counts[d5 - 1]++;
-    var n = 0;
-    var score = 0;
-    for (let i = 0; i < 6; i += 1)
-      if (counts[6 - i - 1] >= 2) {
-        n++;
-        score += 6 - i;
-      }
-    if (n == 2) return score * 2;
-    else return 0;
+    let numberOfRolls = [6, 5, 4, 3, 2, 1]
+      .map(number => [d1, d2, d3, d4, d5].filter(roll => number === roll).length);
+    let firstDice = numberOfRolls.findIndex(x => x >= 2);
+    let secondDice = numberOfRolls.findIndex((x, index) => x >= 2 && index > firstDice);
+
+    return (6 - firstDice) * 2 + (6 - secondDice) * 2;
   }
 
   static four_of_a_kind(_1: number, _2: number, d3: number, d4: number, d5: number): number {
