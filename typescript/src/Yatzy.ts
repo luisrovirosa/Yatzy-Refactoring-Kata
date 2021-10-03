cc6377d7b0c7390dce6b2c7cc56b4ada6c1ebc55
Luis Rovirosa Mairlot <luis@codium.team>
2021-10-03 16:15:40 +0200

    [refactor] Yatzy methods receive Dices

--- typescript/src/Yatzy.ts
+++ typescript/src/Yatzy.ts
@@ -1,68 +1,67 @@
 export default class Yatzy {
-  static chance(roll: Roll): number {
-    return new Dices(...roll).sum();
+  static chance(dices: Dices): number {
+    return dices.sum();
   }
 
-  static yatzy(roll: Roll): number {
-    let isYatzy = Dices.from(roll).findGreaterDiceWithNumberOfSameDices(5) !== 0;
+  static yatzy(dices: Dices): number {
+    let isYatzy = dices.findGreaterDiceWithNumberOfSameDices(5) !== 0;
     return isYatzy ? 50 : 0;
   }
 
-  static ones(roll: Roll): number {
-    return Dices.from(roll).with(1).sum();
+  static ones(dices: Dices): number {
+    return dices.with(1).sum();
   }
 
-  static twos(roll: Roll): number {
-    return Dices.from(roll).with(2).sum();
+  static twos(dices: Dices): number {
+    return dices.with(2).sum();
   }
 
-  static threes(roll: Roll): number {
-    return Dices.from(roll).with(3).sum();
+  static threes(dices: Dices): number {
+    return dices.with(3).sum();
   }
 
-  static fours(roll: Roll): number {
-    return Dices.from(roll).with(4).sum();
+  static fours(dices: Dices): number {
+    return dices.with(4).sum();
   }
 
-  static fives(roll: Roll): number {
-    return Dices.from(roll).with(5).sum();
+  static fives(dices: Dices): number {
+    return dices.with(5).sum();
   }
 
-  static sixes(roll: Roll): number {
-    return Dices.from(roll).with(6).sum();
+  static sixes(dices: Dices): number {
+    return dices.with(6).sum();
   }
 
-  static score_pair(roll: Roll): number {
-    return 2 * Dices.from(roll).findGreaterDiceWithNumberOfSameDices(2);
+  static score_pair(dices: Dices): number {
+    return 2 * dices.findGreaterDiceWithNumberOfSameDices(2);
   }
 
-  static two_pair(roll: Roll): number {
-    let dices = Dices.from(roll);
+  static two_pair(dices: Dices): number {
     let firstDice = dices.findGreaterDiceWithNumberOfSameDices(2);
     let secondDice = dices.findGreaterDiceWithNumberOfSameDices(2, firstDice);
     let isTwoPair = firstDice && secondDice;
     return isTwoPair ? firstDice * 2 + secondDice * 2 : 0;
   }
 
-  static three_of_a_kind(roll: Roll): number {
-    return 3 * Dices.from(roll).findGreaterDiceWithNumberOfSameDices(3);
+  static three_of_a_kind(dices: Dices): number {
+    return 3 * dices.findGreaterDiceWithNumberOfSameDices(3);
   }
 
-  static four_of_a_kind(roll: Roll): number {
-    return 4 * Dices.from(roll).findGreaterDiceWithNumberOfSameDices(4);
+  static four_of_a_kind(dices: Dices): number {
+    return 4 * dices.findGreaterDiceWithNumberOfSameDices(4);
   }
 
-  static smallStraight(roll: Roll): number {
-    return Dices.from(roll).equals(Dices.from([1, 2, 3, 4, 5])) ? 15 : 0;
+  static smallStraight(dices: Dices): number {
+    return dices.equals(Dices.from([1, 2, 3, 4, 5])) ? 15 : 0;
   }
 
-  static largeStraight(roll: Roll): number {
-    return Dices.from(roll).equals(Dices.from([2, 3, 4, 5, 6])) ? 20 : 0;
+  static largeStraight(dices: Dices): number {
+    return dices.equals(Dices.from([2, 3, 4, 5, 6])) ? 20 : 0;
   }
 
-  static fullHouse(roll: Roll): number {
-    let threeOfAKind = Dices.from(roll).findGreaterDiceWithNumberOfSameDices(3);
-    let twoOfAKind = Dices.from(roll).findGreaterDiceWithNumberOfSameDices(2, threeOfAKind);
+  static fullHouse(dices: Dices): number {
+    let threeOfAKind = dices.findGreaterDiceWithNumberOfSameDices(3);
+    let twoOfAKind = dices.findGreaterDiceWithNumberOfSameDices(2, threeOfAKind);
     let isFullHouse = threeOfAKind !== 0 && twoOfAKind !== 0;
     return isFullHouse ? threeOfAKind * 3 + twoOfAKind * 2 : 0;
   }
@@ -70,7 +69,7 @@ export default class Yatzy {
 
 type Roll = [number, number, number, number, number];
 
-class Dices {
+export class Dices {
   private dices: number[];
 
   constructor(...dices: number[]) {
